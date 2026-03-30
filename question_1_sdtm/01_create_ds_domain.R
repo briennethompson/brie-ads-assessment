6ba4922774dc008ec130e63af5fb43ea70ca56b1
briennethompson <briennethompson@gmail.com>
2026-03-30 22:21:51 +0000

    Add tidyverse library, set working directory, and read in controlled terminology

--- question_1_sdtm/01_create_ds_domain.R
+++ question_1_sdtm/01_create_ds_domain.R
@@ -1,13 +1,17 @@
+# Set working directory to project root
+setwd("/cloud/project/brie-ads-assessment")
+
 # Initialize libraries
 library(sdtm.oak)
 library(pharmaverseraw)
 library(pharmaversesdtm)
 library(dplyr)
+library(tidyverse)
 
-# Read in raw DS data 
+# Read in raw disposition data 
 ds_raw <- pharmaverseraw::ds_raw
 
-# Read in SDTM DM data
+# Read in SDTM DM (demog) data
 dm <- pharmaversesdtm::dm
 
 # Create oak_id_vars
@@ -16,3 +20,6 @@ ds_raw <- ds_raw %>%
     pat_var = "PATNUM",
     raw_src = "ds_raw"
   )
+
+# Read in study controlled terminology file
+study_ct <- read_csv("question_1_sdtm/sdtm_ct.csv")

c13612ce63626d0bcc2af28ed4c317c8b922c856
briennethompson <briennethompson@gmail.com>
2026-03-30 22:10:56 +0000

    Read in DM and create oak_id_vars

--- question_1_sdtm/01_create_ds_domain.R
+++ question_1_sdtm/01_create_ds_domain.R
@@ -6,3 +6,13 @@ library(dplyr)
 
 # Read in raw DS data 
 ds_raw <- pharmaverseraw::ds_raw
+
+# Read in SDTM DM data
+dm <- pharmaversesdtm::dm
+
+# Create oak_id_vars
+ds_raw <- ds_raw %>%
+  generate_oak_id_vars(
+    pat_var = "PATNUM",
+    raw_src = "ds_raw"
+  )

503446316d7598f333f322ffd4dad1eba9eaf696
briennethompson <briennethompson@gmail.com>
2026-03-31 21:47:01 +0000

    Generate log for stacked bar plot

--- question_3_tlg/generate_log.R
+++ question_3_tlg/generate_log.R
@@ -7,8 +7,14 @@
 setwd("/cloud/project/brie-ads-assessment")
 
 # Write log file to specified logs folder
+#logrx::axecute(
+#  "question_3_tlg/01_create_ae_summary_table.R",
+#  log_name = "01_create_ae_summary_table.log",
+#  log_path = "question_3_tlg/logs"
+#)
+
 logrx::axecute(
-  "question_3_tlg/01_create_ae_summary_table.R",
-  log_name = "01_create_ae_summary_table.log",
+  "question_3_tlg/02_stacked_bar_plot.R",
+  log_name = "02_stacked_bar_plot.log",
   log_path = "question_3_tlg/logs"
 )

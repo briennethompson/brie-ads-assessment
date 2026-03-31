443612d2a47abf03181f5b2df0e73b4060e03fd6
briennethompson <briennethompson@gmail.com>
2026-03-31 16:18:25 +0000

    Derive agecat vars for ADSL

--- question_2_adam/create_adsl.R
+++ question_2_adam/create_adsl.R
@@ -34,3 +34,21 @@ dm_suppdm <- metatools::combine_supp(dm, suppdm)
 # Use combined demog data as basis for ADSL
 adsl <- dm_suppdm %>%
   select(-DOMAIN)
+
+# Derive age grouping variables - AGEGR9 and AGEGR9N
+# Create lookup table for AGEGR9/AGEGR9N
+agegr9_lookup <- admiral::exprs(
+  ~condition,            ~AGEGR9, ~AGEGR9N,
+  AGE < 18,                "<18",        1,
+  between(AGE, 18, 50),  "18-50",        2,
+  AGE > 50,                ">50",        3,
+  is.na(AGE),          "Missing",        4
+)
+
+# Apply lookup table to working data to derive agecat vars
+adsl <- adsl %>%
+  derive_vars_cat(
+    definition = agegr9_lookup
+  )
+
+

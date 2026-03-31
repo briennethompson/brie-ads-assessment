105b17d6e2b1d5e4d9fe3e2b8652340339f3133b
briennethompson <briennethompson@gmail.com>
2026-03-31 00:09:04 +0000

    Map DSDECOD and DSCAT

--- question_1_sdtm/01_create_ds_domain.R
+++ question_1_sdtm/01_create_ds_domain.R
@@ -33,3 +33,39 @@ ds <-
     id_vars = oak_id_vars()
   )
 
+# Map Qualifiers (DSDECOD AND DSCAT)
+ds <- ds %>%
+  # Map DSDECOD using controlled terminology and condition_add
+  assign_ct(
+    raw_dat = condition_add(ds_raw, is.na(OTHERSP)), #filter raw data
+    raw_var = "IT.DSDECOD",
+    tgt_var = "DSDECOD",
+    ct_spec = study_ct,
+    ct_clst = "C66727",
+    id_vars = oak_id_vars()
+  ) %>%
+  # Map DSCAT without using controlled terminology (missing from the study_ct file) and condition_add
+  hardcode_no_ct(
+    raw_dat = condition_add(ds_raw, IT.DSDECOD == "Randomized"),
+    raw_var = "IT.DSDECOD",
+    tgt_var = "DSCAT",
+    tgt_val = "PROTOCOL MILESTONE",
+    id_vars = oak_id_vars()
+  ) %>%
+  # it doesn't seem that condition_add can handle multiple conditions so DSCAT is broken into 2 calls
+   hardcode_no_ct(
+    raw_dat = condition_add(ds_raw, IT.DSDECOD != "Randomized"),
+    raw_var = "IT.DSDECOD",
+    tgt_var = "DSCAT",
+    tgt_val = "DISPOSITION EVENT",
+    id_vars = oak_id_vars()
+  )
+
+
+
+
+
+
+
+
+
